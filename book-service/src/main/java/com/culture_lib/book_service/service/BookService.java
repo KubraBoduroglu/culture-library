c86e16ccb66146454b21e521c4540d48269c22e6
KubraBoduroglu <k.boduroglu@outlook.com>
2025-12-29 18:38:58 +0300

    added note

--- book-service/src/main/java/com/culture_lib/book_service/service/BookService.java
+++ book-service/src/main/java/com/culture_lib/book_service/service/BookService.java
@@ -11,6 +11,7 @@ import java.util.stream.Collectors;
 
 @Service
 public class BookService {
+    // final çünkü test yazarken bu nesneleri kolayca BookService constructor'ına inject edebilmek için.
     private  final BookRepository repository;
 
     public BookService(BookRepository repository) {
